2c857129eebf4b49be5cd6e17838015c05dbe09e
Adam <57396704+Sankti@users.noreply.github.com>
2020-09-20 14:16:29 +0200

    Creating directory component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,8 @@
 import { getDefaultNormalizer } from '@testing-library/react';
 import React from 'react';
 
+import './menu-item.styles.scss';
+
 const MenuItem = ({ title }) => (
     <div className='menu-item'>
         <div className='content'>
