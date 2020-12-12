1cdd8cd52437aed658bb11eeeeb0ba9e3569c5e2
Adam <57396704+Sankti@users.noreply.github.com>
2020-12-12 14:02:54 +0100

    Setting up user state in App.js to enable usage of user data

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ class App extends React.Component {
     this.state = {
       currentUser: null
     }
+  };
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
   }
 
   render() {
